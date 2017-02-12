2312631fd5ae1cab5525ba03a88f77e0100227f3
pgaret <peregringaret@gmail.com>
2017-02-12 13:29:06 -0500

    Some basic styling, layout is working much better

--- static/js/playlists.js
+++ static/js/playlists.js
@@ -14,17 +14,12 @@ angular.module('playlister')
           if (result.status === 200) {
             $scope.$apply(function(){
               $scope.playlists.push({name: $scope.playlistName, dbId: result.data[0]._id.$oid, videos: []})
-              console.log($scope.playlists)
-              $scope.currentPlaylist = $scope.playlists.length - 1
+              $scope.currentPlaylist = $scope.playlists[$scope.playlists.length - 1]
           }) } else {
             $scope.$apply(function(){ $scope.playlisterror = true; }) } })
     }
-    $scope.changePlaylist = (listName) => {
-      for (let i = 0; i < $scope.playlists.length; i++){
-        if ($scope.playlists[i].name === listName){
-          $scope.currentPlaylist = i
-        }
-      }
+    $scope.changePlaylist = (index) => {
+      $scope.currentPlaylist = $scope.playlists[index]
     }
     $scope.searchVideos = () => {
       let API_KEY= 'AIzaSyDCpSBcCWvzr4mqRS5b6LwYFwD6C9Nx_z4'
@@ -37,34 +32,41 @@ angular.module('playlister')
           })
     }
     $scope.$on('youtube.player.ended', function($event, player){
-      let nextVideo = player.g >= $scope.playlists[$scope.currentPlaylist].videos.length ? 'play1' : 'play'+(player.g+1)
+      let nextVideo = player.g >= $scope.currentPlaylist.videos.length ? 'play1' : 'play'+(player.g+1)
       $timeout(function(){
         // debugger
         document.getElementById(nextVideo).click()
       })
     })
+    $scope.parseName = function(name){
+      if (name.length > 25){
+        return name.substring(0, 25)+"..."
+      } else{
+        return name
+      }
+    }
     $scope.getIframeSrc = function(src){
       return 'https://www.youtube.com/embed/'+src
     }
     $scope.addVideo = (videoId, videoName) => {
-      axios({method: 'POST', url: `/playlists/`+$scope.playlists[$scope.currentPlaylist].dbId,
+      axios({method: 'POST', url: `/playlists/`+$scope.currentPlaylist.dbId,
               data: {'videoId': videoId, 'videoName': videoName}
     }).then(result=>{
       if (result.status === 200) {
         $scope.$apply(function(){
-          $scope.playlists[$scope.currentPlaylist].videos.push({name: videoName, ytId: videoId, dbId: result.data[0]._id.$oid})
+          $scope.currentPlaylist.videos.push({name: videoName, ytId: videoId, dbId: result.data[0]._id.$oid})
         })
       }
       else { $scope.$apply( () =>  { $scope.addVideoError = true }) }
     })}
     $scope.removeVideo = (videoId) => {
-      axios({method: 'DELETE', url: '/playlists/'+$scope.playlists[$scope.currentPlaylist].dbId+"/"+videoId}
+      axios({method: 'DELETE', url: '/playlists/'+$scope.currentPlaylist.dbId+"/"+videoId}
       ).then(result=>{
         if (result.status === 200){
-          for (let i = 0; i < $scope.playlists[$scope.currentPlaylist].videos.length; i++){
-            if ($scope.playlists[$scope.currentPlaylist].videos[i].name === videoId){
+          for (let i = 0; i < $scope.currentPlaylist.videos.length; i++){
+            if ($scope.currentPlaylist.videos[i].name === videoId){
               $scope.$apply(()=>{
-                $scope.playlists[$scope.currentPlaylist].videos.splice(i, 1)
+                $scope.currentPlaylist.videos.splice(i, 1)
                 return null
               })
             }
@@ -80,7 +82,7 @@ angular.module('playlister')
       $scope.email = next.email
       for (let i = 0; i < next.data.length; i++) {$scope.playlists.push({name: next.data[i].name, dbId: next.data[i]._id.$oid, videos: next.data[i].videos})}
       $scope.signedin = true
-      $scope.currentPlaylist = 0
+      $scope.currentPlaylist = $scope.playlists[0]
     })
     $scope.$on('signedout', function(){ $scope.signedin = false })
     $scope.printScope = () => { console.log($scope) }
